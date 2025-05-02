a608c9af0d7c98b1a39db57d6714bb5d743a01eb
freemanzok <ali.zokaei.1367@gmail.com>
2025-05-02 19:36:32 +0330

    save me

--- script.js
+++ script.js
@@ -28,8 +28,12 @@ $(document).ready(function(){
         cssEase: 'linear',
         autoplay: true,
         autoplaySpeed: 5000,
-        prevArrow: '<button type="button" class="slick-prev">Previous</button>',
-        nextArrow: '<button type="button" class="slick-next">Next</button>',
+        prevArrow: `<button type="button" class="slick-prev flex justify-center items-center">
+        <?xml version="1.0" encoding="UTF-8"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-chevron-left" width="24" height="24" viewBox="0 0 24 24"><path d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z" />
+        // </svg></button>`,
+        nextArrow:`<button type="button" style="color:red" class="slick-next  flex justify-center items-center">
+        <?xml version="1.0" encoding="UTF-8"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-chevron-right" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
+        </button>`,
         responsive: [
             {
                 breakpoint: 768,
